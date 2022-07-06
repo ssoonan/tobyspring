2e30fb114421c92e23e97b31a833ba4a8c1752cb
ssoonan <ssoonan0770@gmail.com>
2022-07-06 14:41:56 +0900

    구현: 팩토리 xml로 변경
    구현: 팩토리 xml로 변경

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,5 +1,5 @@
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -7,15 +7,16 @@ public class UserDaoTest {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         try {
             dao.add(new User("asd", "ssoonan", "1234"));
             User user = dao.get("asd");
             System.out.println(user.name);
-            CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
-            System.out.println("count: " + ccm.counter);
+//            CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+//            System.out.println("count: " + ccm.counter);
             dao.delete("asd");
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
